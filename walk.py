90844341985ee8358c705c0029d80af1605ab419
aliburkemper12 <aliburkemper12@gmail.com>
2025-10-06 21:27:39 -0500

    test21

--- walk.py
+++ walk.py
@@ -54,11 +54,11 @@ class Walk(Node):
 		elif self.leftwhisker < 0.5 and self.rightwhisker > 0.5:
 			# Obstacle on left — turn right
 			self.move_cmd.linear.x = 0.3
-			self.move_cmd.angular.z = -0.1
+			self.move_cmd.angular.z = -0.3
 		elif self.rightwhisker < 0.5 and self.leftwhisker > 0.5:
 			# Obstacle on right — turn left
 			self.move_cmd.linear.x = 0.3
-			self.move_cmd.angular.z = 0.1
+			self.move_cmd.angular.z = 0.3
 		elif self.whisker < 2.0:
 			# Turn away from closer side
 			if self.leftwhisker > self.rightwhisker:
